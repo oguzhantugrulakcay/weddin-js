957d651974b302e3ddb47084099f5c55577e534e
Oğuzhantuğrul Akçay <oakcay@artisoft.com.tr>
2025-11-21 13:42:47 +0300

    control

--- app/page.js
+++ app/page.js
@@ -8,27 +8,42 @@ export default function Home() {
   const timelineEvents = [
     { 
       id: 1, 
-      title: "Düğün Töreni", 
+      title: "Nişan Töreni", 
       description: "03 Ocak 2026, Cumartesi - Saat 19:00", 
-      location: "The Marmara Taksim, İstanbul",
+      location: "Teras Dragos, Kartal/İstanbul",
       date: new Date("2026-01-03T19:00:00"),
       image: "images/bride.jpg"
     },
     { 
       id: 2, 
-      title: "Nikah Töreni", 
-      description: "03 Ocak 2026, Cumartesi - Saat 17:00", 
-      location: "The Marmara Taksim, İstanbul",
-      date: new Date("2026-01-03T17:00:00"),
+      title: "Düğün Töreni", 
+      description: "19 Temmuz 2026, Pazar - Saat 19:00", 
+      location: "Suare Garden VIP Salonu, Tuzla/İstanbul",
+      date: new Date("2026-07-19T19:00:00"),
       image: "images/bride.jpg"
     },
     { 
       id: 3, 
       title: "Kına Gecesi", 
-      description: "02 Ocak 2026, Cuma - Saat 20:00", 
+      description: "15 Temmuz 2026, Cuma - Saat 20:00", 
       location: "The Marmara Taksim, İstanbul",
-      date: new Date("2026-01-02T20:00:00"),
+      date: new Date("2026-07-15T20:00:00"),
       image: "images/bride.jpg"
+    },{
+      id: 4,
+      title:"Tanıştık",
+      description:"Kartal Atalar Sürücü Kursunda eğitmen ve öğrenci olarak tanıştık.",
+      location:"Kartal/İstanbul",
+      date: new Date("2024-06-15T10:00:00"),
+      image:"images/cover_bg_1.jpg"
+    }
+    ,{
+      id: 5,
+      title:"Sevgili olduk",
+      description:"Ben flört sevmiyorum!!!.",
+      location:"Kartal/İstanbul",
+      date: new Date("2024-09-28T10:00:00"),
+      image:"images/cover_bg_1.jpg"
     }
   ];
 
